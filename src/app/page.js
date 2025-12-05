eaf0274a6b383836c4c53f8f7bbf75cff4360c44
Tamara Ujmajuridze <Tauj0001@stud.kea.dk>
2025-12-05 14:29:33 +0100

    header

--- src/app/page.js
+++ src/app/page.js
@@ -2,16 +2,22 @@ import Hero from "./Components/Hero";
 import Sektion1 from "./Components/Sektion1";
 import Sektion2 from "./Components/Sektion2";
 import Sektion3 from "./Components/Sektion3";
+import Newsletter from "./Components/Newsletter";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
 
 const Home = () => {
   return (
     <>
       <header>
         <Hero />
+        <Header />
       </header>
       <Sektion1 />
       <Sektion2 />
       <Sektion3 />
+      <Newsletter />
+      <Footer />
     </>
   );
 };
