3412800b50a65a872e7f38030fb63581cb4af123
Line Madsen <lima0001@stud.kea.dk>
2025-12-09 17:57:52 +0100

    audio

--- src/app/page.js
+++ src/app/page.js
@@ -7,6 +7,7 @@ import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import Testimonials from "./Components/Testimonials";
 import RecentBlog from "./Components/RecentBlog";
+import Audio from "./Components/Audio";
 
 const Home = () => {
   return (
@@ -18,6 +19,7 @@ const Home = () => {
       <Sektion1 />
       <Sektion2 />
       <Sektion3 />
+      <Audio />
       <Testimonials />
       <RecentBlog />
       <Newsletter />
