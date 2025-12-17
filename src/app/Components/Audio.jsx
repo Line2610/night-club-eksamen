4a0893c7d5a8408c54e0cff2631dd8e272783343
Line Madsen <lima0001@stud.kea.dk>
2025-12-17 13:51:11 +0100

    ændring på texr

--- src/app/Components/Audio.jsx
+++ src/app/Components/Audio.jsx
@@ -80,7 +80,7 @@ export default function App() {
   return (
     <div className="bg-black py-12">
       <div className="max-w-7xl mx-auto px-6">
-        <h2 className="text-white text-4xl text-center tracking-widest mb-20">
+        <h2 className="text-white text-2xl text-center tracking-widest mb-20">
           NIGHT CLUB TRACK
                   <Image src="/assets/bottom_line2.png" width={400} height={50} alt="decorative line" className="mx-auto block" />
         </h2>
@@ -200,9 +200,9 @@ export default function App() {
           {mobileOverlay && (
             <>
               {/* Top left triangle */}
-              <div className="absolute top-0 left-0 w-0 h-0 border-l-50 border-l-[#FF2A70] border-b-50 border-b-transparent z-10" />
+              <div className="absolute top-0 left-0 w-0 h-0 border-l-60 border-l-[#FF2A70] border-b-60 border-b-transparent z-10" />
               {/* Bottom right triangle */}
-              <div className="absolute bottom-0 right-0 w-0 h-0 border-r-50 border-r-[#FF2A70] border-t-50 border-t-transparent z-10" />
+              <div className="absolute bottom-0 right-0 w-0 h-0 border-r-60 border-r-[#FF2A70] border-t-60 border-t-transparent z-10" />
               {/* Play button overlay */}
               <div className="absolute inset-0 flex items-center justify-center z-20">
                 <Image src="/assets/icon/Play_btn.svg" alt="Play Button" width={56} height={56} />
